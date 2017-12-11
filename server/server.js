14d273b56d6e72a6a9f2fd0bc37ae12634517174
Saumil Bapat <sbapat@salesforce.com>
2017-12-11 14:35:40 -0500

    Add Get Todos Id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
@@ -7,7 +8,7 @@ const {User} = require("./models/user");
 
 var app = express();
 app.use(bodyParser.json());
-app.post('/Todos', (req, res) => {
+app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text,
     });
@@ -18,7 +19,7 @@ app.post('/Todos', (req, res) => {
         console.log('Failed to save, ', err);
     });
 });
-app.get('/Todos', (req, res) => {
+app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({
             todos
@@ -26,6 +27,24 @@ app.get('/Todos', (req, res) => {
     }).catch((err) => {
         console.log(err);
         res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const todoId = req.params.id;
+    if(!ObjectID.isValid(todoId)) {
+        res.status(400);
+        res.send('Invalid Id');
+    }
+    Todo.findById(todoId).then((todo) => {
+        if(!todo){
+            res.status(404);
+            res.send('Todo not found');
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400);
+        res.send('Exception: ', err.message);
     });
 });
 
