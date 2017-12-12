0932c3d9e579c59753667f152d46d16091cfe659
Saumil Bapat <sbapat@salesforce.com>
2017-12-11 22:38:55 -0500

    Added patch request

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -75,6 +76,31 @@ app.delete('/todos/:id', (req, res) => {
             .status(400)
             .send('Exception: ' + err.message);
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(400).send('Invalid id');
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send('Todo not found');
+        }
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send(err.message);
+    })
 });
 
 app.listen(port, () => {
