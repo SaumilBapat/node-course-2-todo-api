7966dc7f926fdca56987dbb26fbc27144084d52e
Saumil Bapat <sbapat@salesforce.com>
2017-12-24 23:40:19 -0500

    password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 let message = "I am user number 3";
 let hash = SHA256(message);
@@ -10,6 +11,15 @@ console.log(`hash: ${hash}`);
 let data = {
     id: 4,
 };
+let password = '123abc!';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(`hash: ${hash}`);
+        bcrypt.compare(password, hash, (err, res) => {
+            console.log(`res; ${res}`);
+        })
+    });
+});
 
 //let token = jwt.sign(data, '123abc');
 //console.log(token);
